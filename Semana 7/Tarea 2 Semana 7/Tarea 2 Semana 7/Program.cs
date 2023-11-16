b2c2d899c4932f1be90e0461f5ca0119cb0d90cc
Ignaciogg04 <151039419+Ignaciogg04@users.noreply.github.com>
2023-11-16 08:11:39 -0600

    Subiendo actividades Portafolio

---
+++ Semana 7/Tarea 2 Semana 7/Tarea 2 Semana 7/Program.cs
@@ -0,0 +1,137 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_2_Semana_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n_Monto = 0, n_Descuento = 0, n_Tarjeta = 0, n_DescuentoTarjeta = 0, n_MontoF = 0, n_Total = 0;
+            Console.WriteLine("¿Usted cuenta con código de descuento? Si = 1, No = 0");
+            n_Tarjeta = Convert.ToDouble(Console.ReadLine());
+
+            if (n_Tarjeta == 1)
+            {
+                Console.WriteLine("Si cuenta con código de descuento");
+                n_DescuentoTarjeta = 0.05;
+                Console.WriteLine("Ingrese el monto de su compra");
+                n_Monto = Convert.ToDouble(Console.ReadLine());
+                if (n_Monto <= 0)
+                {
+                    Console.WriteLine("ERROR");
+                }
+                else if (n_Monto < 400)
+                {
+                    n_Descuento = 0;
+                    n_MontoF = n_Monto - ((n_DescuentoTarjeta + n_Descuento) * n_Monto);
+                    n_Total = (n_DescuentoTarjeta + n_Descuento) * 100;
+
+                    Console.WriteLine("El descuento total aplicado es del " + n_Total + "%");
+                    Console.WriteLine("El monto final es de Q" + n_MontoF);
+                }
+                else if (n_Monto <= 1000)
+                {
+                    n_Descuento = 0.07;
+                    n_MontoF = n_Monto - ((n_DescuentoTarjeta + n_Descuento) * n_Monto);
+                    n_Total = (n_DescuentoTarjeta + n_Descuento) * 100;
+
+                    Console.WriteLine("El descuento total aplicado es del " + n_Total + "%");
+                    Console.WriteLine("El monto final es de Q" + n_MontoF);
+                }
+                else if (n_Monto <= 5000)
+                {
+                    n_Descuento = 0.1;
+                    n_MontoF = n_Monto - ((n_DescuentoTarjeta + n_Descuento) * n_Monto);
+                    n_Total = (n_DescuentoTarjeta + n_Descuento) * 100;
+
+                    Console.WriteLine("El descuento total aplicado es del " + n_Total + "%");
+                    Console.WriteLine("El monto final es de Q" + n_MontoF);
+                }
+                else if (n_Monto <= 15000)
+                {
+                    n_Descuento = 0.15;
+                    n_MontoF = n_Monto - ((n_DescuentoTarjeta + n_Descuento) * n_Monto);
+                    n_Total = (n_DescuentoTarjeta + n_Descuento) * 100;
+
+                    Console.WriteLine("El descuento total aplicado es del " + n_Total + "%");
+                    Console.WriteLine("El monto final es de Q" + n_MontoF);
+                }
+                else if (n_Monto > 15000)
+                {
+                    n_Descuento = 0.25;
+                    n_MontoF = n_Monto - ((n_DescuentoTarjeta + n_Descuento) * n_Monto);
+                    n_Total = (n_DescuentoTarjeta + n_Descuento) * 100;
+
+                    Console.WriteLine("El descuento total aplicado es del " + n_Total + "%");
+                    Console.WriteLine("El monto final es de Q" + n_MontoF);
+                }
+            }
+            else if (n_Tarjeta == 0)
+            {
+                Console.WriteLine("No cuenta con código de descuento");
+                n_DescuentoTarjeta = 0;
+                Console.WriteLine("Ingrese el monto de su compra");
+                n_Monto = Convert.ToDouble(Console.ReadLine());
+                if (n_Monto <= 0)
+                {
+                    Console.WriteLine("ERROR");
+                }
+                else if (n_Monto < 400)
+                {
+                    n_Descuento = 0;
+                    n_MontoF = n_Monto - ((n_DescuentoTarjeta + n_Descuento) * n_Monto);
+                    n_Total = (n_DescuentoTarjeta + n_Descuento) * 100;
+
+                    Console.WriteLine("El descuento total aplicado es del " + n_Total + "%");
+                    Console.WriteLine("El monto final es de Q" + n_MontoF);
+                }
+                else if (n_Monto <= 1000)
+                {
+                    n_Descuento = 0.07;
+                    n_MontoF = n_Monto - ((n_DescuentoTarjeta + n_Descuento) * n_Monto);
+                    n_Total = (n_DescuentoTarjeta + n_Descuento) * 100;
+
+                    Console.WriteLine("El descuento total aplicado es del " + n_Total + "%");
+                    Console.WriteLine("El monto final es de Q" + n_MontoF);
+                }
+                else if (n_Monto <= 5000)
+                {
+                    n_Descuento = 0.1;
+                    n_MontoF = n_Monto - ((n_DescuentoTarjeta + n_Descuento) * n_Monto);
+                    n_Total = (n_DescuentoTarjeta + n_Descuento) * 100;
+
+                    Console.WriteLine("El descuento total aplicado es del " + n_Total + "%");
+                    Console.WriteLine("El monto final es de Q" + n_MontoF);
+                }
+                else if (n_Monto <= 15000)
+                {
+                    n_Descuento = 0.15;
+                    n_MontoF = n_Monto - ((n_DescuentoTarjeta + n_Descuento) * n_Monto);
+                    n_Total = (n_DescuentoTarjeta + n_Descuento) * 100;
+
+                    Console.WriteLine("El descuento total aplicado es del " + n_Total + "%");
+                    Console.WriteLine("El monto final es de Q" + n_MontoF);
+                }
+                else if (n_Monto > 15000)
+                {
+                    n_Descuento = 0.25;
+                    n_MontoF = n_Monto - ((n_DescuentoTarjeta + n_Descuento) * n_Monto);
+                    n_Total = (n_DescuentoTarjeta + n_Descuento) * 100;
+
+                    Console.WriteLine("El descuento total aplicado es del " + n_Total + "%");
+                    Console.WriteLine("El monto final es de Q" + n_MontoF);
+                }
+            }
+            else
+            {
+                Console.WriteLine("ERROR");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Semana 7/vidad 1 semana 7/vidad 1 semana 7/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vidad_1_semana_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int c;
+            int d;
+            int e;
+            int f;
+            double g;
+            double h;
+            double i;
+
+            Console.WriteLine("Ingrese la primera cantidad");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Si la cantidad es en quetzales, escriba 1; si la cantidad es en dólares, escriba 2");
+            d = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la segunda cantidad");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Si la cantidad es en quetzales, escriba 1; si la cantidad es en dólares, escriba 2");
+            e = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la tercera cantiadad");
+            c = int.Parse(Console.ReadLine());
+            Console.WriteLine("Si la cantidad es en quetzales, escriba 1; si la cantidad es en dólares, escriba 2");
+            f = int.Parse(Console.ReadLine());
+
+
+            if (d == 2)
+            {
+                g = (a * 7.83);
+            }
+            else
+            {
+                g = a;
+            }
+
+            if (e == 2)
+            {
+                h = (b * 7.83);
+            }
+            else
+            {
+                h = b;
+            }
+
+            if (f == 2)
+            {
+                i = (c * 7.83);
+            }
+            else
+            {
+                i = c;
+            }
+
+            if ((a < b) && (a < c))
+            {
+                Console.WriteLine("El número menor es: " + g);
+            }
+            else if ((a > b) && (a < c))
+            {
+                Console.WriteLine("El número de en medio es: " + g);
+            }
+
+            else if ((a < b) && (a < c))
+            {
+                Console.WriteLine("El número de en medio es: " + g);
+            }
+            else
+            {
+                Console.WriteLine("El número mayor es: " + g);
+            }
+            if ((b < a) && (b < c))
+            {
+                Console.WriteLine("El número menor es: " + h);
+            }
+            else if ((b > a) && (b < c))
+            {
+                Console.WriteLine("El número de en medio es: " + h);
+            }
+            else if ((b < c) && (b < c))
+            {
+                Console.WriteLine("El número de en medio es: " + h);
+            }
+            else
+            {
+                Console.WriteLine("El número mayor es: " + h);
+            }
+
+            if ((c < b) && (c < a))
+            {
+                Console.WriteLine("El número menor es: " + i);
+            }
+            else if ((c > b) && (c < a))
+            {
+                Console.WriteLine("El número de en medio es: " + i);
+            }
+
+            else if ((c < b) && (c < a))
+            {
+                Console.WriteLine("El número de en medio es: " + i);
+            }
+            else
+            {
+                Console.WriteLine("El número mayor es: " + i);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
